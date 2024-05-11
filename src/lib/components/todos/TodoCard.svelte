bad771c1e457c278827c3a4951c6c978c2e6e971
shotakvitsashvilii <shota@lemondo.biz>
2024-05-12 02:12:40 +0400

    checbox render condition change

--- src/lib/components/todos/TodoCard.svelte
+++ src/lib/components/todos/TodoCard.svelte
@@ -88,12 +88,14 @@
         }
     }
 
-    let isExpired= parse(expireDate as string, 'MM/dd/yyyy - HH:mm', new Date()).getTime() < new Date().getTime()
+    let isExpired = parse(expireDate as string, 'MM/dd/yyyy - HH:mm', new Date()).getTime() < new Date().getTime(),
+    isFinished = status === todoStatusEnums.completed,
+    isOngoing = status === todoStatusEnums.ongoing
 
 
     let todoStatus = classnames({
-        'Finished': status === todoStatusEnums.completed && !isExpired,
-        'Ongoing': status === todoStatusEnums.ongoing && !isExpired,
+        'Finished': isFinished && !isExpired,
+        'Ongoing': isOngoing && !isExpired,
         'Expired': isExpired
     })
 </script>
@@ -114,63 +116,63 @@ class="{
         `,
         {
             '!bg-blue-gray-50': editable,
-            'bg-green-100': status === todoStatusEnums.completed,
-            'bg-yellow-100': status === todoStatusEnums.ongoing,
-            'bg-red-100/50': isExpired,
+            'bg-green-100': isFinished,
+            'bg-yellow-100': isOngoing,
+            '!bg-red-100/50': isExpired,
         }
     )
 }
 ">
     <form
-    class="flex items-stretch gap-4 "
-    class:valid={$isValid}
-    onsubmit={handleSubmit}
->
-    <div
-        class="flex-1 {editable
-            ? ''
-            : 'opacity-90 pointer-events-none cursor-default'}"
+        class="flex items-stretch gap-4 "
+        class:valid={$isValid}
+        onsubmit={handleSubmit}
     >
-        <div class="-ml-4 [&_textarea]:text-lg">
-            <Input
-                type="textarea"
-                name="todoBody"
-                {handleChange}
-                {errors}
-                value={todoBody}
-                readOnly={!editable}
-            />
-        </div>
-
-       {#if status !== todoStatusEnums.completed}
-            <div class="[&_input]:!bg-transparent [&_input]:!text-gray-600 flex items-center gap-2 text-sm">
-                {isExpired ? 'Expired' : 'Expires'} at: 
-                <Datepicker {updateField} defaultDate={expireDate} hideLabel />
+        <div
+            class="flex-1 {editable
+                ? ''
+                : 'opacity-90 pointer-events-none cursor-default'}"
+        >
+            <div class="-ml-4 [&_textarea]:text-lg">
+                <Input
+                    type="textarea"
+                    name="todoBody"
+                    {handleChange}
+                    {errors}
+                    value={todoBody}
+                    readOnly={!editable}
+                />
             </div>
-       {/if}
-       
 
-        <div class="my-1">
-            <h6
-                class="block font-sans text-sm antialiased font-[600] leading-relaxed tracking-normal text-blue-gray-900"
-            >
-                Priority
-            </h6>
+        {#if !isFinished}
+                <div class="[&_input]:!bg-transparent [&_input]:!text-gray-600 flex items-center gap-2 text-sm">
+                    {isExpired ? 'Expired' : 'Expires'} at: 
+                    <Datepicker {updateField} defaultDate={expireDate} hideLabel />
+                </div>
+        {/if}
+        
 
-            <div
-                class="flex flex-col md:flex-row md:items-center gap-2 md:gap-3"
-            >
-                {#each todoRadioFields as field}
-                    <Input
-                        {...field}
-                        {handleChange}
-                        {errors}
-                        defaultChecked={field.value === priority}
-                    />
-                {/each}
+            <div class="my-1">
+                <h6
+                    class="block font-sans text-sm antialiased font-[600] leading-relaxed tracking-normal text-blue-gray-900"
+                >
+                    Priority
+                </h6>
+
+                <div
+                    class="flex flex-col md:flex-row md:items-center gap-2 md:gap-3"
+                >
+                    {#each todoRadioFields as field}
+                        <Input
+                            {...field}
+                            {handleChange}
+                            {errors}
+                            defaultChecked={field.value === priority}
+                        />
+                    {/each}
+                </div>
             </div>
         </div>
-    </div>
 
     <div class="flex flex-col justify-between">
         <div class="flex items-center gap-2">
@@ -179,13 +181,13 @@ class="{
             <button
                 type="submit"
                 onclick={(e) => {
-                    if(status !== todoStatusEnums.completed) {
+                    if(!isFinished) {
                         !editable && e.preventDefault();
                         editable = !editable;
                         requestDelete = false;
                     }
                 }}
-                class={status === todoStatusEnums.completed ? 'cursor-not-allowed' : ''}
+                class={isFinished ? 'cursor-not-allowed' : ''}
             >
                 <img
                     src={editable ? checkIcon : editIcon}
@@ -205,45 +207,48 @@ class="{
                 }}
             />
         </div>
-        <div class="inline-flex items-center">
-            <label
-                class="relative flex items-center p-3 rounded-full cursor-pointer {editable ? 'pointer-events-none' : ''}"
-            >
-                <input
-                    type="checkbox"
-                    onchange={(e: Event) => {
-                        const checked = (e.target as HTMLInputElement).checked;
-
-                        updateTodo("statusUpdate", {
-                                ...todo,
-                                status: checked ? todoStatusEnums.completed : todoStatusEnums.ongoing,
-                                finishedAt: checked? format(new Date(Date.now()), 'MM/dd/yyyy - HH:mm') : null
-                            })
-                    }}
-                    class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
-                    id="checkbox"
-                    checked={status === todoStatusEnums.completed}
-                />
-                <span
-                    class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100"
+       {#if !isExpired}
+            <div class="inline-flex items-center">
+                <label
+                    class="relative flex items-center p-3 rounded-full cursor-pointer {editable ? 'pointer-events-none' : ''}"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-3.5 w-3.5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        stroke="currentColor"
-                        stroke-width="1"
+                    <input
+                        type="checkbox"
+                        onchange={(e: Event) => {
+                            const checked = (e.target as HTMLInputElement).checked;
+
+                            updateTodo("statusUpdate", {
+                                    ...todo,
+                                    status: checked ? todoStatusEnums.completed : todoStatusEnums.ongoing,
+                                    finishedAt: checked? format(new Date(Date.now()), 'MM/dd/yyyy - HH:mm') : null
+                                })
+                        }}
+                        class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                        id="checkbox"
+                        checked={isFinished}
+                    />
+                    <span
+                        class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100"
                     >
-                        <path
-                            fill-rule="evenodd"
-                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                            clip-rule="evenodd"
-                        ></path>
-                    </svg>
-                </span>
-            </label>
-        </div>        
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-3.5 w-3.5"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            stroke="currentColor"
+                            stroke-width="1"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                    </span>
+                </label>
+            </div> 
+       {/if}
+             
     </div>
 </form>
 
